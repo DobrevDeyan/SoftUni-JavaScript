0f531090213d5d3619c8cf3fa39b87172525ccd3
DobrevDeyan <38318634+DobrevDeyan@users.noreply.github.com>
2021-09-14 22:04:11 +0300

    Added new exercise

--- JS Advanced/2. Arrays and Nested Arrays/Exercise.js
+++ JS Advanced/2. Arrays and Nested Arrays/Exercise.js
@@ -94,40 +94,199 @@ function sortingNumbers(arr) {
     //         a.push(tArr[i--])
     //         return a
     //     }, [])
-    //     .slice(0, arr.length)
+    //     .slice(0, arr.length);
+
 
     let newArray = [];
-    let num = 0;
+    let arrayLength = arr.length;
 
-    arr = arr.filter((_, i) => {
+    for (let i = 0; i < arrayLength; i++) {
+        let number;
         if (i % 2 === 0) {
-            num = Math.max(...arr);
+            number = Math.min(...arr);
         } else {
-            num = Math.min(...arr);
+            number = Math.max(...arr);
         }
-        newArray.push(num);
-        arr.splice(arr.indexOf(num), 1);
+        newArray.push(number);
+        arr.splice(arr.indexOf(number), 1);
+    }
+    return newArray;
+
+
+    // let newArray = [];
+    // let num = 0;
+    // let i = arr.length - 1;
+    //
+    // arr = arr.filter((a, b, i) => {
+    //     if (i % 2 === 0) {
+    //         num = Math.min(...arr);
+    //         newArray.push(num);
+    //         arr.splice(arr.indexOf(num), 1);
+    //
+    //     } else {
+    //         num = Math.max(...arr);
+    //         newArray.push(num);
+    //         arr.splice(arr.indexOf(num), 1);
+    //     }
+    // });
+    //
+    // console.log(newArray)
+}
+// sortingNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);
+
+
+
+function sortArrayByCriteria(arr) {
+    let sorted = arr.sort((a, b) => {
+        // return a.length - b.length === 0 ? a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase()) : a.length - b.length;
+        if (a.length - b.length === 0) {
+            return a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase());
+        } else {
+            return a.length - b.length;
+        }
     });
 
-    for (let newArrayElement of newArray) {
-        console.log(newArrayElement);
+    for (let sortedElement of sorted) {
+        console.log(sortedElement);
     }
 }
-sortingNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);
+// sortArrayByCriteria(['alpha', 'beta', 'gamma']);
+// sortArrayByCriteria(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']);
+// sortArrayByCriteria(['test', 'Deny', 'omen', 'Default']);
 
 
 
+function magicMatrices(arr) {
+    return [
+        ...arr.map(x => x.reduce((a, v) => a + v, 0)),
+        ...arr.reduce((a, v, i) => {
+            a.push(arr.reduce((c, d) => c + d[i], 0))
+            return a
+        }, []),
+    ].every((x, _, arr) => x === arr[0])
+}
+// magicMatrices([[4, 5, 6], [6, 5, 4], [5, 5, 5]]);
+// magicMatrices([[11, 32, 45], [21, 0, 1], [21, 1, 1]]);
+// magicMatrices([[1, 0, 0], [0, 0, 1], [0, 1, 0]]);
 
 
 
+function ticTacToe(arr) {
+    const board = [0, 0, 0].map(x => ["false", "false", "false"])
+    let player = "X"
+
+    function isWinningMove(board, row, place) {
+        // after the move has been made, we -->
+        // check current row for 3 equals
+        // || reducing to new array with column === the place player is marking.
+        //e.g. if player is on place 2 in row 1,
+        //we make new array with all places 2 from all 3 rows
+        //to check for column equality
+        // || adding main diagonal to new array, checking it for equality
+        // || adding second diagonal and checking it.
+        return (
+            board[row].every(x => x === player) ||
+            board
+                .reduce((a, v) => {
+                    a.push(v[place])
+                    return a
+                }, [])
+                .every(x => x === player) ||
+            [board[0][0], board[1][1], board[2][2]].every(x => x === player) ||
+            [board[0][2], board[1][1], board[2][0]].every(x => x === player)
+        )
+    }
+
+    const printBoard = board => console.log(board.map(x => x.join("\t")).join("\n"))
+
+    for (let i = 0; i < arr.length; i++) {
+        const [row, place] = arr[i].split(" ").map(Number)
+
+        if (board[row][place] !== "false") {
+            console.log("This place is already taken. Please choose another!")
+            continue
+        }
+        board[row][place] = player
+
+        if (isWinningMove(board, row, place)) {
+            console.log(`Player ${player} wins!`)
+            printBoard(board)
+            break
+        }
+
+        if (board.every(x => x.every(y => y !== "false"))) {
+            console.log("The game ended! Nobody wins :(")
+            printBoard(board)
+            break
+        }
+
+        player = player === "X" ? "O" : "X"
+    }
+}
+// ticTacToe(["0 1", "0 0", "0 2", "2 0", "1 0", "1 1", "1 2", "2 2", "2 1", "0 0"]);
+// ticTacToe(["0 0", "0 0", "1 1", "0 1", "1 2", "0 2", "2 2", "1 2", "2 2", "2 1"]);
+// ticTacToe(["0 1", "0 0", "0 2", "2 0", "1 0", "1 2", "1 1", "2 1", "2 2", "0 0"]);
 
 
 
+function diagonalAttack(arr) {
+    arr = arr.map(x => x.split(" ").map(Number))
+    const getDiagSum = arr => arr.reduce((a, v, i) => a + v[i], 0)
+    const mapToSum = (arr, sum) =>
+        arr.map((x, i) => x.map((y, j) => (j === i || j === x.length - i - 1 ? y : sum)))
+    const print = arr => arr.map(x => x.join(" ")).join("\n")
+    const [reversedMatrix, resultMatrix] = [arr.reverse(), arr.reverse()]
+    return getDiagSum(resultMatrix) === getDiagSum(reversedMatrix)
+        ? print(mapToSum(resultMatrix, getDiagSum(resultMatrix)))
+        : print(resultMatrix)
+}
+// diagonalAttack(['5 3 12 3 1', '11 4 23 2 5', '101 12 3 21 10', '1 4 5 2 2', '5 22 33 11 1']);
+// diagonalAttack(['1 1 1', '1 1 1', '1 1 0']);
 
 
 
+function orbit(arr) {
+    let matrix = new Array(arr[0]).fill(new Array(arr[1]).fill(''))
+
+    return matrix
+        .map((x, i) => x.map((y, j) => Math.max(Math.abs(j - arr[3]), Math.abs(i - arr[2])) + 1))
+        .map(x => x.join(" "))
+        .join("\n")
+}
+// orbit([4, 4, 0, 0]);
+// orbit([5, 5, 2, 2]);
+// orbit([3, 3, 2, 2]);
 
 
+
+function spiralMatrix(w, h) {
+    let matrix = new Array(h).fill(new Array(w).fill([]))
+    matrix = matrix.map(x => x.map(y => ""))
+    let [startRow, endRow, startClmn, endClmn, counter] = [0, h - 1, 0, w - 1, 0]
+
+    while (startClmn <= endClmn && startRow <= endRow) {
+        for (let i = startClmn; i <= endClmn; i++) {
+            matrix[startRow][i] = ++counter
+        }
+        startRow++
+        for (let i = startRow; i <= endClmn; i++) {
+            matrix[i][endClmn] = ++counter
+        }
+        endClmn--
+        for (let i = endClmn; i >= startClmn; i--) {
+            matrix[endRow][i] = ++counter
+        }
+        endRow--
+        for (let i = endRow; i >= startRow; i--) {
+            matrix[i][startClmn] = ++counter
+        }
+        startClmn++
+    }
+
+    return matrix.map(x => x.join(" ")).join("\n")
+}
+// spiralMatrix(5, 5);
+// spiralMatrix(3, 3);
 
 
 
