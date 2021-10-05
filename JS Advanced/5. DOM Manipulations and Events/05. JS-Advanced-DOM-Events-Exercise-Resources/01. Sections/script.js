25004d23f5265976b0af18620469a70c4d4fb64d
DobrevDeyan <38318634+DobrevDeyan@users.noreply.github.com>
2021-10-05 11:11:18 +0300

    New lessons added

--- JS Advanced/5. DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Exercise-Resources/01. Sections/script.js
+++ JS Advanced/5. DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Exercise-Resources/01. Sections/script.js
@@ -1,3 +1,21 @@
 function create(words) {
-   console.log('TODO:...');
+    const content = document.getElementById("content");
+
+    for (let word of words) {
+        const div = document.createElement("div");
+        const paragraph = document.createElement("p");
+
+        paragraph.textContent = word;
+        paragraph.style.display = "none";
+
+        div.appendChild(paragraph);
+        div.addEventListener("click", reveal);
+        content.appendChild(div);
+    }
+
+    function reveal(event) {
+        if (event.target.tagName === "DIV" && event.target !== content) {
+            event.currentTarget.children[0].style.display = "";
+        }
+    }
 }
--- JS Advanced/5. DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Exercise-Resources/03. Locked-Profile/app.js
+++ JS Advanced/5. DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Exercise-Resources/03. Locked-Profile/app.js
@@ -1,3 +1,30 @@
 function lockedProfile() {
-    console.log('TODO...')
+    Array.from(document.querySelectorAll('.profile button')).
+    forEach(b => b.addEventListener('click', onToggle));
+
+    function onToggle(event) {
+        const profile = event.target.parentElement;
+        const isActive = profile.querySelector('input[type="radio"][value="unlock"]').checked;
+
+        if (isActive) {
+            const infoDif = Array
+                .from(event.target.parentElement.querySelectorAll('div'))
+                .find(d => d.id.includes('HiddenFields'));
+
+            if (event.target.textContent === 'Show more') {
+                infoDif.style.display = 'block';
+                event.target.textContent = 'Hide it';
+            } else {
+                infoDif.style.display = '';
+                event.target.textContent = 'Show more';
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
