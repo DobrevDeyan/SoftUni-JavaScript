18113fa3450732b2db5757ec04c17ca6649732fe
Deyan Dobrev <dobrev.de@gmail.com>
2021-10-17 13:36:06 +0300

    commit

---
+++ JS Advanced/7. Unit Testing and Error Handling/test-demo/Lab.js
@@ -0,0 +1,21 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function product(a, b) {
+    return a * b;
+}
+
+const data = [1, 2, 3];
+
+function printArray() {
+    console.log(data);
+}
+
+module.exports = {
+    sum,
+    product,
+    data,
+    printArray
+};
+
