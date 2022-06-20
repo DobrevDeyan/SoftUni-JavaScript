2f904bfe3151b50ca3a5af025e736e709e4a002d
deyandobrev <dobrev.de@gmail.com>
2022-06-20 18:46:58 +0300

    updatedTaks

--- JS Advanced/6. Advanced Functions/Lab.js
+++ JS Advanced/6. Advanced Functions/Lab.js
@@ -1,19 +1,39 @@
-function contextPrint() {
-  console.log(this);
+function solve(area, volume, input) {
+  return JSON.parse(input).map((item) => ({
+    area: area.apply(item),
+    volume: volume.apply(item),
+  }));
+
+  // const data = JSON.parse(input);
+
+  // const resultArr = [];
+  // for (let item of data) {
+  //   const cubeArea = area.apply(item);
+  //   const cubeVolume = volume.apply(item);
+  //   resultArr.push({ area: cubeArea, volume: cubeVolume });
+  // }
+  // return resultArr;
 }
 
-function myFunc() {
-  obj.contextPrint();
+function area() {
+  return Math.abs(this.x * this.y);
 }
+function volume() {
+  return Math.abs(this.x * this.y * this.z);
+}
+// console.log(
+//   solve(
+//     area,
+//     volume,
+//     `[
+//   {"x":"10","y":"-22","z":"10"},
+//   {"x":"47","y":"7","z":"-5"},
+//   {"x":"55","y":"8","z":"0"},
+//   {"x":"100","y":"100","z":"100"},
+//   {"x":"55","y":"80","z":"250"}
+//   ]`
+//   )
+// );
 
-const obj = {
-  name: "Peter",
-  contextPrint,
-};
-// obj.contextPrint();
-// myFunc();
 
-const extractedFunc = obj.contextPrint;
-// extractedFunc();
 
-document.querySelector("button").addEventListener("click", obj.contextPrint);
