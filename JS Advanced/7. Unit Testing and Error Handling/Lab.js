028b58dc46b8dfd0af526ad9b5df1774f7759abb
deyandobrev <dobrev.de@gmail.com>
2022-06-26 11:47:49 +0300

    lab-tasks

--- JS Advanced/7. Unit Testing and Error Handling/Lab.js
+++ JS Advanced/7. Unit Testing and Error Handling/Lab.js
@@ -69,3 +69,47 @@ function playingCards(face, suit) {
 // console.log(testTwo.toString());
 // const testThree = playingCards("9", "Y");
 // console.log(testThree.toString());
+
+// 3. Deck of Cards
+function deckOfCards() {
+  function playingCards(face, suit) {
+    const faces = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+      "A",
+    ];
+    const suits = {
+      S: "\u2660",
+      H: "\u2665",
+      D: "\u2666",
+      C: "\u2663",
+    };
+
+    if (faces.includes(face) === false) {
+      throw new Error("Invalid face: " + face);
+    }
+    if (Object.keys(suits).includes(suit) === false) {
+      throw new Error("Invalid Suit: " + suit);
+    }
+
+    return {
+      face,
+      suit: suits[suit],
+      toString() {
+        return this.face + this.suit;
+      },
+    };
+  }
+}
+deckOfCards(["AS", "10D", "KH", "2C"]);
+deckOfCards(["5S", "3D", "QD", "1C"]);
