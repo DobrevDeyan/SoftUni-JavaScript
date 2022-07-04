d9ddb0e38f0d096b6b8eda83dfc0e5611b0630cd
deyandobrev <dobrev.de@gmail.com>
2022-07-04 12:43:46 +0300

    asynbLabTasks

--- JS Applications/1. Asynchronous Programming/03.Gighub-Commits/app.js
+++ JS Applications/1. Asynchronous Programming/03.Gighub-Commits/app.js
@@ -1,4 +1,25 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+  const username = document.getElementById("username");
+  const repo = document.getElementById("repo");
+  const commits = document.getElementById("commits");
+  commits.innerHTML = "";
+
+  try {
+    const dataPromise = await fetch(
+      `https://api.github.com/repos/${username.value}/${repo.value}/commits`
+    );
+    if (!dataPromise.ok) {
+      throw new Error(`${data.status} ${data.statusText}`);
+    }
+    const receivedData = await dataPromise.json();
+    receivedData.forEach((commit) => {
+      const liItem = document.createElement("li");
+      liItem.textContent = `${commit.commit.author.name}: ${commit.commit.message}`;
+      commits.appendChild(liItem);
+    });
+  } catch (error) {
+    const liItem = document.createElement("li");
+    liItem.textContent = `Error: ${error} (Not Found)`;
+    commits.appendChild(liItem);
+  }
 }
