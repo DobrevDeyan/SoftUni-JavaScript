d7f6e0250a5b92d2452eff56b0bfc96f8e1c44b8
deyandobrev <dobrev.de@gmail.com>
2022-07-04 10:41:02 +0300

    asynbLabTasks

--- JS Applications/1. Asynchronous Programming/02.Github-Repos/scripts/app.js
+++ JS Applications/1. Asynchronous Programming/02.Github-Repos/scripts/app.js
@@ -1,3 +1,26 @@
 function loadRepos() {
-	console.log("TODO...");
+  const userName = document.getElementById("username").value;
+  const list = document.getElementById("repos");
+  const url = `https://api.github.com/users/${userName}/repos`;
+
+  fetch(url)
+    .then((res) => {
+      if (res.ok == false) {
+        throw new Error(`${res.status} ${res.statusText}`);
+      }
+      return res.json();
+    })
+    .then(handleResponse)
+    .catch((error) => console.log(error));
+
+  function handleResponse(data) {
+    // list.innerHTML = "";
+    list.querySelectorAll("*").forEach((n) => n.remove());
+
+    for (let repo of data) {
+      const liElement = document.createElement("li");
+      liElement.innerHTML = `<a href="${repo.html_url}"> ${repo.full_name} </a>`;
+      list.appendChild(liElement);
+    }
+  }
 }
