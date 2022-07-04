04ab3aee21d9f346d6e0b631dc0ee385328763c9
deyandobrev <dobrev.de@gmail.com>
2022-07-04 14:23:57 +0300

    asynbExerciseTasks

--- JS Applications/1. Asynchronous Programming/Exercise/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
+++ JS Applications/1. Asynchronous Programming/Exercise/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
@@ -1,3 +1,27 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+  const busStop = document.getElementById("stopId").value;
+  const stopNameElement = document.getElementById("stopName");
+  const timeTable = document.getElementById("buses");
+  const button = document.getElementById("submit");
+  const url = `http://localhost:3030/jsonstore/bus/businfo/${busStop}`;
+
+  try {
+    stopNameElement.textContent = "Loading...";
+    button.setAttribute("disabled", "");
+    timeTable.replaceChildren();
+    const response = await fetch(url);
+    if (response.status !== 200) {
+      throw new Error("Stop ID not found!");
+    }
+    const data = await response.json();
+    stopNameElement.textContent = data.name;
+    Object.entries(data.buses).forEach((bus) => {
+      const li = document.createElement("li");
+      li.textContent = `Bus ${bus[0]} arrives in ${bus[1]} minutes`;
+      timeTable.appendChild(li);
+    });
+    button.removeAttribute("disabled");
+  } catch (error) {
+    stopNameElement.textContent = "Error";
+  }
 }
