dd73db94109b8896bf33f1a30e0653f16e1be041
deyandobrev <dobrev.de@gmail.com>
2022-07-05 16:05:12 +0300

    asyncExerciseTasks

--- JS Applications/1. Asynchronous Programming/Exercise/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/03.Forecaster/app.js
+++ JS Applications/1. Asynchronous Programming/Exercise/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/03.Forecaster/app.js
@@ -1,5 +1,36 @@
 function attachEvents() {
-    console.log("TODO...");
+  const forecastButton = document.getElementById("submit");
+  forecastButton.addEventListener("click", getLocation);
 }
-
 attachEvents();
+
+async function getLocation() {
+  const locationInput = document.getElementById("location").value;
+  const getLocationInfo = await fetch(
+    "http://localhost:3030/jsonstore/forecaster/locations"
+  );
+  const locationData = await getLocationInfo.json();
+  locationData.map((location) => {
+    const { code, name } = location;
+    if (name === locationInput) {
+      currentWeatherForecast(code);
+      threeDayWeatherForecast(code);
+    }
+  });
+}
+async function currentWeatherForecast(code) {
+  const response = await fetch(
+    `http://localhost:3030/jsonstore/forecaster/today/${code}`
+  );
+  const data = await response.json();
+  //   return data;
+  console.log(data);
+}
+async function threeDayWeatherForecast(code) {
+  const response = await fetch(
+    `http://localhost:3030/jsonstore/forecaster/upcoming/${code}`
+  );
+  const data = await response.json();
+  //   return data;
+  console.log(data);
+}
