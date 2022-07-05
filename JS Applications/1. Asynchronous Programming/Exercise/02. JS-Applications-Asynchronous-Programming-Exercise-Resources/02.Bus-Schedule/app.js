93c604baaf64e32c97bc8417f679fd72d518614a
deyandobrev <dobrev.de@gmail.com>
2022-07-05 09:15:37 +0300

    asyncExerciseTasks

--- JS Applications/1. Asynchronous Programming/Exercise/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
+++ JS Applications/1. Asynchronous Programming/Exercise/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
@@ -1,17 +1,40 @@
 function solve() {
+  const arriveButton = document.getElementById("arrive");
+  const departButton = document.getElementById("depart");
+  const informationBox = document.getElementById("info");
+  const url = `http://localhost:3030/jsonstore/bus/schedule/${depot}`;
 
-    function depart() {
-        console.log('Depart TODO...');
+  async function depart() {
+    try {
+      const firstBusStop = await fetch url;
+      if (firstBusStop.ok !== 200) {
+        // return firstBusStop.json();
+        console.log(firstBusStop.json());
+      } else {
+        throw new Error("Request failed");
+      }
+    } catch (error) {
+      console.log(error.message);
     }
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+    arriveButton.disabled = true;
+    departButton.disabled = false;
 
-    return {
-        depart,
-        arrive
-    };
+    informationBox.innerHTML = `Next stop ${2}`;
+  }
+
+  function arrive() {
+    // try {
+    // } catch (error) {}
+
+    arriveButton.disabled = false;
+    departButton.disabled = true;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
