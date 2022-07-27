3d2ac78c2426fff2bdb5763a114e48fe976acdb4
deyandobrev <dobrev.de@gmail.com>
2022-07-27 14:00:06 +0300

    SPAExercise

--- JS Applications/4. Architecture and Testing/demo-test/test.js
+++ JS Applications/4. Architecture and Testing/demo-test/test.js
@@ -15,10 +15,10 @@ const { expect } = require("chai")
 let browser, page // Declare reusable variables
 
 describe("E2E tests", async function () {
-  // this.timeout(5000)
+  // this.timeout(10000)
 
   before(async () => {
-    browser = await chromium.launch()
+    browser = await chromium.launch({ headless: false, slowMo: 2000 })
   })
   after(async () => {
     await browser.close()
@@ -46,7 +46,9 @@ describe("E2E tests", async function () {
   })
 
   it("More button works", async () => {
-    await page.goto("http://localhost:5500")
+    await page.goto(
+      "http://127.0.0.1:5500/4.%20Architecture%20and%20Testing/05.%20JS-Applications-Architecture-and-Testing-Lab-Resources/01.%20Accordion/index.html"
+    )
     await page.waitForSelector(".accordion")
 
     await page.click("text=More")
@@ -54,5 +56,29 @@ describe("E2E tests", async function () {
     await page.waitForSelector(".accordion p")
     const visible = await page.isVisible(".accordion p")
     expect(visible).to.be.true
+  })
+
+  it("Less button works", async () => {
+    await page.goto(
+      "http://127.0.0.1:5500/4.%20Architecture%20and%20Testing/05.%20JS-Applications-Architecture-and-Testing-Lab-Resources/01.%20Accordion/index.html"
+    )
+    await page.waitForSelector(".accordion")
+
+    await page.click(`text=More`)
+    await page.waitForResponse(/articles\/details/i)
+    await page.waitForSelector(".accordion p", { state: "visible" })
+
+    await page.click("text=Less")
+
+    const visible = await page.isVisible(".accordion p")
+    expect(visible).to.be.false
+  })
+
+  it.only("form inputs work", async () => {
+    await page.goto(
+      "http://127.0.0.1:5500/4.%20Architecture%20and%20Testing/05.%20JS-Applications-Architecture-and-Testing-Lab-Resources/01.%20Accordion/index.html"
+    )
+    await page.fill("[name='email']", "Peter")
+    await page.waitForTimeout(60000)
   })
 })
