1655ccd66b01d8744a5e90850a5c4c492a994ec7
deyandobrev <dobrev.de@gmail.com>
2022-08-03 13:11:29 +0300

    architectureExercise

--- JS Applications/4. Architecture and Testing/05. JS-Applications-Architecture-and-Testing-Exercise-Resources/02.Book-Library/test.js
+++ JS Applications/4. Architecture and Testing/05. JS-Applications-Architecture-and-Testing-Exercise-Resources/02.Book-Library/test.js
@@ -1,14 +1,37 @@
 const { chromium } = require("playwright-chromium")
 const { expect } = require("chai")
 
+const mockData = {
+  "d953e5fb-a585-4d6b-92d3-ee90697398a0": {
+    author: "J.K.Rowling",
+    title: "Harry Potter and the Philosopher's Stone",
+  },
+  "d953e5fb-a585-4d6b-92d3-ee90697398a1": {
+    author: "Svetlin Nakov",
+    title: "C# Fundamentals",
+  },
+}
+
+function json(data) {
+  return {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }
+}
+
 describe("tests", async function () {
   // we don't use arrow func since this (context) has to be set on the place of execution (not creation)
-  this.timeout(5000)
+  this.timeout(50000)
 
   let page, browser
 
   before(async () => {
     browser = await chromium.launch()
+    // browser = await chromium.launch({ headless: false, slowMo: 2000 })
   })
   after(async () => {
     await browser.close()
@@ -21,13 +44,36 @@ describe("tests", async function () {
     await page.close()
   })
 
-  it("works", async () => {
+  //FIRST TEST
+  it("loads and displays all books", async () => {
     // await new Promise((res) => setTimeout(res, 2000)) // simulate waiting in que with a promise
-    // expect(1).to.equal(1)
+    // await page.screenshot({ path: "page.png" })
+    await page.route("**/jsonstore/collections/books*", (route) => {
+      route.fulfill(json(mockData))
+    })
 
     await page.goto(
-      "http://127.0.0.1:5500/JS%20Applications/4.%20Architecture%20and%20Testing/05.%20JS-Applications-Architecture-and-Testing-Exercise-Resources/02.Book-Library/index.html"
+      "http://127.0.0.1:5500/JS%20Applications/4.%20Architecture%20and%20Testing/05.%20JS-Applications-Architecture-and-Testing-Exercise-Resources/02.Book-Library"
     )
-    await page.screenshot({ path: "page.png" })
+
+    await page.click("text=Load All Books")
+    await page.waitForSelector("text=Harry Potter")
+
+    const rows = await page.$$eval("tr", (rows) =>
+      rows.map((row) => row.textContent.trim())
+    )
+
+    expect(rows[1]).to.contains("Harry Potter")
+    expect(rows[1]).to.contains("Rowling")
+    expect(rows[2]).to.contains("C# Fundamentals")
+    expect(rows[2]).to.contains("Nakov")
+  })
+
+  //SECOND TEST
+  it("can create book", async () => {
+    await page.goto(
+      "http://127.0.0.1:5500/JS%20Applications/4.%20Architecture%20and%20Testing/05.%20JS-Applications-Architecture-and-Testing-Exercise-Resources/02.Book-Library"
+    )
+    
   })
 })
