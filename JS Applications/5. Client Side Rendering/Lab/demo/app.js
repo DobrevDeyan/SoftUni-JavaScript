7f36eb38f75ed4733f73858c1b38d3a406d46bef
deyandobrev <dobrev.de@gmail.com>
2022-08-06 16:45:15 +0300

    clientSideRendering

--- JS Applications/5. Client Side Rendering/Lab/demo/app.js
+++ JS Applications/5. Client Side Rendering/Lab/demo/app.js
@@ -2,9 +2,12 @@ import { renderTemplate } from "./engine.js"
 
 async function start() {
   const data = await (await fetch("./data.json")).json()
-  const template = await (await fetch("./article.html")).text()
+  //   const template = await (await fetch("./article.html")).text()
+  const templateAsString = await (await fetch("./article.html")).text()
 
+  const template = renderTemplate(templateAsString)
   const main = document.querySelector("main")
-  main.innerHTML = data.map((item) => renderTemplate(template, item)).join("")
+  //   main.innerHTML = data.map((item) => renderTemplate(template, item)).join("")
+  main.innerHTML = data.map(template).join("")
 }
 start()
--- JS Applications/5. Client Side Rendering/Lab/demo/engine.js
+++ JS Applications/5. Client Side Rendering/Lab/demo/engine.js
@@ -1,7 +1,12 @@
 export function renderTemplate(templateAsString, data) {
   const pattern = /{{(.+?)}}/gm // global, multiline
 
-  return templateAsString.replace(pattern, (match, propName) => {
-    return data[propName]
-  })
+  //   return templateAsString.replace(pattern, (match, propName) => {
+  //     return data[propName]
+  //   })
+  return (data) => {
+    return templateAsString.replace(pattern, (match, propName) => {
+      return data[propName]
+    })
+  }
 }
