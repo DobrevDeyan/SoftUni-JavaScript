9f4b05e4d38e22b228d140fc75c5c014facaa76d
deyandobrev <dobrev.de@gmail.com>
2022-08-08 19:06:42 +0300

    examPreparation

--- JS Applications/ExamPreparation/Meme_Lounge/src/app.js
+++ JS Applications/ExamPreparation/Meme_Lounge/src/app.js
@@ -1,10 +1,10 @@
 import { page, render } from "./lib.js"
 import { homePage } from "./views/home.js"
-import { catalogPage } from "./views/catalog.js"
+// import { catalogPage } from "./views/catalog.js"
 
 // below for debugging in console
-import * as api from "./api/data.js"
-window.api = api
+// import * as api from "./api/data.js"
+// window.api = api
 // above for debugging in console
 
 const root = document.querySelector("main")
@@ -18,6 +18,5 @@ page.start()
 
 function decorateContext(ctx, next) {
   ctx.render = (content) => render(content, root)
-
   next()
 }
